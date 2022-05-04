b59c01fe5db9761c81baef7041c9bf0ccd435d58
mariavand <mariamasha.vandoulaki@gmail.com>
2022-05-04 18:32:02 +0300

    finished login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AuthModule } from './auth/auth.module';
+import { AuthService } from './shared/services/auth.service';
 import { UsersService } from './shared/services/users.service';
 
 @NgModule({
@@ -19,7 +20,7 @@ import { UsersService } from './shared/services/users.service';
     AppRoutingModule,
     AuthModule
   ],
-  providers: [UsersService],
+  providers: [UsersService, AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { Message } from 'src/app/shared/models/message.model';
 import { User } from 'src/app/shared/models/user.model';
+import { AuthService } from 'src/app/shared/services/auth.service';
 import { UsersService } from 'src/app/shared/services/users.service';
 
 @Component({
@@ -15,7 +17,9 @@ export class LoginComponent implements OnInit {
   form!: FormGroup;
   message!: Message;
 
-  constructor(private userService:UsersService) { }
+  constructor(private userService:UsersService, 
+    private authService: AuthService, 
+    private router: Router) { }
 
   ngOnInit(): void {
     this.message = new Message('danger', '');
@@ -39,8 +43,10 @@ export class LoginComponent implements OnInit {
       .subscribe((user: User) => 
         {if(user){
           if(user.password === formData.password){
-            this.showMessage('Correct', 'success');
-            //logic
+            this.message.text = '';
+            window.localStorage.setItem('user', JSON.stringify(user));
+            this.authService.login();
+            //this.router.navigate(['']);
           }
           else{
             this.showMessage('Incorrect password');
